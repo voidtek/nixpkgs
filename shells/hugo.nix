1fa883fae727f5b56f17bd8c11a23cf67cf17b72
voidtek <vdacosta@voidtek.com>
2026-01-03 00:57:51 +0000

    Add tool for images on hugo

--- shells/hugo.nix
+++ shells/hugo.nix
@@ -5,6 +5,8 @@ pkgs.mkShellNoCC {
     pkgs.hugo
     pkgs.nodejs
     pkgs.go
+    pkgs.libwebp
+    pkgs.imagemagick
   ];
 
   shellHook = ''
@@ -18,7 +20,7 @@ pkgs.mkShellNoCC {
     echo "╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝"
     echo ""
     echo "Welcome to the Hugo development environment!"
-    echo "Available tools: wget, curl, shellcheck, unzip, htop, btop, nano, ncdu, cacert, gnupg, git, hugo, nodejs, go"
+    echo "Available tools: wget, curl, shellcheck, unzip, htop, btop, nano, ncdu, cacert, gnupg, git, hugo, nodejs, go, libwebp, imagemagick"
     echo ""
 
     ${fishLaunch}
